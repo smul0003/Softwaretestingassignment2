abb8917b09722b838ab46a2377b0129a751e42e4
Maurie93 <turc0022@flinders.edu.au>
2025-06-15 22:47:07 +0930

    Car Test Completed

--- test/CarTest.java
+++ test/CarTest.java
@@ -12,7 +12,9 @@ class CarTest {
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 14.210526315789474",
                 "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 17.416666666666668",
-                "Standard, Hard, Low-Drag Kit"})
+                "Standard, Hard, Low-Drag Kit, 125, 7.5, 22.500000000000004",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0, 17.647058823529413",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0, 21.65625"})
     @DisplayName("Total Fuel Efficiency Test")
     void getTotalFuelEfficiency(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, double expected) {
         Engine engine = EngineFactory.create(engineType);
@@ -24,7 +26,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 78.0",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 55.0"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 55.0",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5, 142.0",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0, 82.0",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0, 108.0"})
     @DisplayName("Top Speed Test")
     void getTopSpeed(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, double expected) {
         Engine engine = EngineFactory.create(engineType);
@@ -36,7 +41,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 6",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 9"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 9",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5, 5",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0, 7",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0, 7"})
     @DisplayName("Cornering Ability Test")
     void getCorneringAbility(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, int expected) {
         Engine engine = EngineFactory.create(engineType);
@@ -48,7 +56,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 6.5",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 7.75"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 7.75",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5, 6.25",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0, 6.5",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0, 7.5"})
     @DisplayName("Total Handling Test")
     void getTotalHandling(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, double expected) {
         Engine engine = EngineFactory.create(engineType);
@@ -60,7 +71,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 8",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 7"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 7",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5, 6",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0, 6",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0, 7"})
     @DisplayName("Acceleration Test")
     void getAcceleration(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, int expected) {
         Engine engine = EngineFactory.create(engineType);
@@ -72,7 +86,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 100",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 75"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 75",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5, 125",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0, 150",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0, 50"})
     @DisplayName("Fuel Tank Capacity Test")
     void getFuelTankCapacity(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, int expected) {
         Engine engine = EngineFactory.create(engineType);
@@ -84,7 +101,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0"})
     @DisplayName("Tyre Test")
     void getTyre(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling) {
         Engine engine = EngineFactory.create(engineType);
@@ -96,7 +116,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0"})
     @DisplayName("Engine Test")
     void getEngine(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling) {
         Engine engine = EngineFactory.create(engineType);
@@ -108,7 +131,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5"})
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5",
+                "Standard, Hard, Low-Drag Kit, 125, 7.5",
+                "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0",
+                "Hybrid, Hard, Ground Effect Kit, 50, 8.0"})
     @DisplayName("AeroKit Test")
     void getAeroKit(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling) {
         Engine engine = EngineFactory.create(engineType);
@@ -120,7 +146,10 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, Turbocharged, Medium, Standard Kit, 100, 78, 14.21, 6.50",
-            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, Hybrid, Soft, Downforce-Focused Kit, 75, 55, 17.42, 7.75"})
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, Hybrid, Soft, Downforce-Focused Kit, 75, 55, 17.42, 7.75",
+            "Standard, Hard, Low-Drag Kit, 125, 7.5, Standard, Hard, Low-Drag Kit, 125, 142, 22.50, 6.25",
+            "notAnEngine, notATyre, Adjustable Aero Kit, 150, 6.0, Standard, Medium, Adjustable Aero Kit, 150, 82, 17.65, 6.50",
+            "Hybrid, Hard, Ground Effect Kit, 50, 8.0, Hybrid, Hard, Ground Effect Kit, 50, 108, 21.66, 7.50"})
     @DisplayName("To String Test")
     void testToString(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, String s1, String s2, String s3, String s4, String s5, String s6, String s7) {
         Engine engine = EngineFactory.create(engineType);
