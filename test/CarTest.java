729d81ef810a88965c06f954a315ca147392b8fb
Maurie93 <turc0022@flinders.edu.au>
2025-06-15 22:21:29 +0930

    Car Test needs more parameterisations

---
+++ test/CarTest.java
@@ -0,0 +1,134 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 14.210526315789474",
+                "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 17.416666666666668",
+                "Standard, Hard, Low-Drag Kit"})
+    @DisplayName("Total Fuel Efficiency Test")
+    void getTotalFuelEfficiency(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, double expected) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(expected, car.getTotalFuelEfficiency());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 78.0",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 55.0"})
+    @DisplayName("Top Speed Test")
+    void getTopSpeed(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, double expected) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(expected, car.getTopSpeed());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 6",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 9"})
+    @DisplayName("Cornering Ability Test")
+    void getCorneringAbility(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, int expected) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(expected, car.getCorneringAbility());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 6.5",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 7.75"})
+    @DisplayName("Total Handling Test")
+    void getTotalHandling(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, double expected) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(expected , car.getTotalHandling());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 8",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 7"})
+    @DisplayName("Acceleration Test")
+    void getAcceleration(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, int expected) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(expected, car.getAcceleration());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, 100",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, 75"})
+    @DisplayName("Fuel Tank Capacity Test")
+    void getFuelTankCapacity(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, int expected) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(expected, car.getFuelTankCapacity());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5"})
+    @DisplayName("Tyre Test")
+    void getTyre(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(tyre, car.getTyre());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5"})
+    @DisplayName("Engine Test")
+    void getEngine(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(engine, car.getEngine());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5"})
+    @DisplayName("AeroKit Test")
+    void getAeroKit(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        assertEquals(aeroKit, car.getAeroKit());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Turbocharged, Medium, Hybrid Kit, 100, 7.0, Turbocharged, Medium, Standard Kit, 100, 78, 14.21, 6.50",
+            "Hybrid, Soft, Downforce-Focused Kit, 75, 6.5, Hybrid, Soft, Downforce-Focused Kit, 75, 55, 17.42, 7.75"})
+    @DisplayName("To String Test")
+    void testToString(String engineType, String tyreType, String aeroKitType, int fuelTankCapacity, double baseHandling, String s1, String s2, String s3, String s4, String s5, String s6, String s7) {
+        Engine engine = EngineFactory.create(engineType);
+        Tyre tyre = TyreFactory.create(tyreType);
+        AeroKit aeroKit = AeroKitFactory.create(aeroKitType);
+        Car car = new Car(engine, tyre, aeroKit, fuelTankCapacity, baseHandling);
+        String result = "Car[Engine=" + s1 + ", Tyre=" + s2 + ", AeroKit=" + s3 + ", FuelTank=" + s4 + "L, Speed=" + s5 + "km/h, Efficiency=" + s6 + "km/l, Handling=" + s7 + "/10]";
+        assertEquals(result, car.toString());
+
+    }
+}
